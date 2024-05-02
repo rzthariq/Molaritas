d48a97927e3403bb23986a5f5b59adfcb555717d
rzthariq <168729572+rzthariq@users.noreply.github.com>
2024-05-02 17:29:03 +0700

    Add files via upload

---
+++ lpk 1.py
@@ -0,0 +1,12 @@
+
+massa_molekul=st.number_input('masukkan massa molekul')
+st.write('Massa molekul adalah', massa_molekul)
+
+Faktor_pengali=st.number_input('masukkan faktor pengali')
+st.write('Faktor pengali adalah', Faktor_pengali)
+
+Volume=st.number_input('masukkan volume')
+st.write('Volume adalah', Volume)
+
+Berat_molekul=st.number_input('masukkan berat molekul')
+st.write('Berat molekul adalah', Berat_molekul)
